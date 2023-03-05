2115acb4bff29368fcb17a6f3c8f33b7ec8e259e
Deepesh Gupta <34032722+deepesh-dg@users.noreply.github.com>
2023-03-05 14:16:20 +0530

    design minor change

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -6,14 +6,12 @@ import style from "./Feed.module.scss";
 const Feed = ({ videos }: { videos: IVideoItem[] }) => {
     return (
         <div className={style.feed}>
-            <div className="container-fluid">
-                <div className="row g-3">
-                    {videos.map((video) => (
-                        <div key={video.id} className="col-12">
-                            <VideoCard videoDetails={video} orientation="landscape" description channelIcon={false} />
-                        </div>
-                    ))}
-                </div>
+            <div className="row g-3">
+                {videos.map((video) => (
+                    <div key={video.id} className="col-12">
+                        <VideoCard videoDetails={video} orientation="landscape" description channelIcon={false} />
+                    </div>
+                ))}
             </div>
         </div>
     );
